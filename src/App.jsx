b02e009f1151cdf3b9dfa9871f04b52ef8c12a3b
Austin Imbastari <fvckimba@Austins-MBP.lan>
2022-12-15 10:12:29 -0800

    creating loading screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,35 @@
 import { useState } from "react";
 import { Configuration, OpenAIApi } from "openai";
+import Loader from "./components/Loader";
 import "./App.css";
 
 function App() {
     const [prompt, setPrompt] = useState("");
-    const [result, setResult] = useState(null);
-    const configuration = new Configuration({
-        apiKey: "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",
-    });
+    const [result, setResult] = useState([]);
+    // const [loading, setLoading] = useState(false);
 
+    const configuration = new Configuration({
+        apiKey: "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",
+    });
     const openai = new OpenAIApi(configuration);
     const generateImage = async () => {
         const res = await openai.createImage({
             prompt: prompt,
             n: 1,
-            size: "520x520",
+            size: "512x512",
         });
-
         console.log(res.data.data[0].url);
-        setResult(res.data.data[0].url);
+        setResult([...result, res.data.data[0].url]);
+        // setLoading(!loading);
     };
 
     return (
         <div className='App'>
+            <div className='nav-bar'>
+                <nav>
+                    <h1>Free The Mind</h1>
+                </nav>
+            </div>
             <div className='main-container'>
                 <div className='input-container'>
                     <p>
@@ -35,10 +42,20 @@ function App() {
                             setPrompt(e.target.value);
                         }}
                     />
-                    <button onClick={generateImage}>Generate Image</button>
+                    <button onClick={generateImage}>
+                        <h3>CREATE</h3>
+                    </button>
                 </div>
 
-                <img src={result} alt='' />
+                <div className='img-grid-container'>
+                    {result.map((res) => {
+                        return (
+                            <div className='img-container'>
+                                <img src={res} alt='' />
+                            </div>
+                        );
+                    })}
+                </div>
             </div>
         </div>
     );
